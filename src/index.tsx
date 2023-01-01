acdb9496c9d214dceaecfe5767c838a1cb531b41
jihye <jiji.pak41@gmail.com>
2023-01-01 21:55:57 +0000

    🚧 Create router

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Books from './pages/Books';
+import Form from './pages/Form';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/all', element: <Books /> },
+      { path: '/add', element: <Form /> },
+      { path: '/login', element: <Login /> },
+      { path: '/signup', element: <Signup /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />{' '}
   </React.StrictMode>
 );
